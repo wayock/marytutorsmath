ca60fa0f1dfa2a7a75777b464170264cf613fef6
Matthew Wayock <wayock@gmail.com>
2019-09-03 22:57:47 -0400

    sendgrid api key added

--- src/app.js
+++ src/app.js
@@ -3,6 +3,21 @@ const app = express();
 const appConfig = require("./config/main-config.js");
 const routeConfig = require("./config/route-config.js");
 
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+
+// const msg = {
+//   to: 'marywayock@gmail.com',
+//   from: 'marytutorsmath@math.com',
+//   subject: 'New Tutoring Request',
+//   text: ' ',
+//   html: ' ',
+// };
+// sgMail.send(msg);
+
+
+
 appConfig.init(app, express);
 routeConfig.init(app);
 
