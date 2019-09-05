ff7876655dd9fd0f63ec427e6371d1256114f0d1
Matthew Wayock <wayock@gmail.com>
2019-09-04 21:58:44 -0400

    trying to handle post request www.youtube.com/watch?v=rin7gb9kdpk

--- src/app.js
+++ src/app.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const app = express();
+var bodyParser = require('body-parser');
 const appConfig = require("./config/main-config.js");
 const routeConfig = require("./config/route-config.js");
-
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.post('static/index', urlencodedParser, function (req, res){
+  console.log(req.body);
+  res.render('/', {qs: req.query});
+});
 
 
 // const msg = {
