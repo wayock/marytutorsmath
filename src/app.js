5ce05b8054cbe8fbae4c2a75e470f2563d7704f8
Matthew Wayock <wayock@gmail.com>
2019-09-07 16:41:13 -0400

    form data emailed to client's email

--- src/app.js
+++ src/app.js
@@ -5,7 +5,10 @@ const appConfig = require("./config/main-config.js");
 const routeConfig = require("./config/route-config.js");
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+const flash = require('express-flash-notification');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 // var api_key = process.env.MAILGUN_API_KEY;
 // //var domain = 'sandboxfe530aa7650e4fba98e7ef18a3f061a2.mailgun.org';
 // var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
@@ -48,21 +51,27 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.post('/form', urlencodedParser, function (req, res){
   console.log(req.body);
-  res.render('/', {qs: req.query});
+
+  const msg = {
+    to: 'marywayock@gmail.com',
+    from: 'marytutorsmath@math.com',
+    subject: 'New Tutoring Request',
+    text: 'Please help me learn math!',
+     html: `Parent name: ${req.body.inputParentFirstName4} ${req.body.inputParentLastName4} <br />
+  Student Name: ${req.body.inputStudentFirstName4} ${req.body.inputStudentLastName4} <br />
+  Email: ${req.body.inputEmail4} <br />
+  Phone: ${req.body.phone4} <br />
+  Grade: ${req.body.grade4} <br />
+  Math Subject: ${req.body.inputMathSubject4} <br />
+  Details: ${req.body.inputDetails4}`
+  };
+  sgMail.send(msg);
+  //req.flash('success', 'You have successfully submitted your form.  Mary will be in contact with you in the near future.')
+  res.redirect("/");
+
 });
 
-const msg = {
-  to: 'marywayock@gmail.com',
-  from: 'marytutorsmath@math.com',
-  subject: 'New Tutoring Request',
-  text: 'Please help me learn math!',
-  html: `${req.body.inputParentFirstName4} ${req.body.inputParentLastName4}
-${req.body.inputStudentFirstName4} ${req.body.inputStudentLastName4}
-${req.body.inputEmail4} ${req.body.phone4}
-${req.body.grade4} ${req.body.inputMathSubject4}
-${req.body.inputDetails4}`
-};
-sgMail.send(msg);
+
 
 
 
