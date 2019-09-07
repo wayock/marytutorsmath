3ac7a433c4392a74120bb48afd00fb5db51a347b
Matthew Wayock <wayock@gmail.com>
2019-09-07 14:16:34 -0400

    trying mailgun, get message in terminal but no email

--- src/app.js
+++ src/app.js
@@ -6,33 +6,44 @@ const routeConfig = require("./config/route-config.js");
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 var api_key = process.env.MAILGUN_API_KEY;
-var domain = 'sandboxfe530aa7650e4fba98e7ef18a3f061a2.mailgun.org';
+//var domain = 'sandboxfe530aa7650e4fba98e7ef18a3f061a2.mailgun.org';
 var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
 
+var domain = 'sandboxcfa063f4789041efa5b313db627c9e69.mailgun.org';
+//Your sending email address
+var from_who = 'wayock.com';
+var domain = 'https://wayock-marytutorsmath.herokuapp.com/';
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-app.post('/form', function (req, res){
+app.post('/', function (req, res){
 
-  var data = {
-    from: 'Mail Gun Mary Tutors Math<postmaster@sandboxfe530aa7650e4fba98e7ef18a3f061a2.mailgun.org>',
-    to: 'mwayock@gmail.com',
+   var data =   {
+    from: 'Mail Gun Mary Tutors Math<postmaster@sandboxcfa063f4789041efa5b313db627c9e69.mailgun.org>',
+    to: 'marywayock@gmail.com',
     subject: 'Tutoring Request',
-    text: `${req.body.inputParentFirstName4} ${req.body.inputParentLastName4}
-    ${req.body.inputStudentFirstName4} ${req.body.inputStudentLastName4}
-    ${req.body.inputEmail4} ${req.body.phone4}
-    ${req.body.grade4} ${req.body.inputMathSubject4}
-    ${req.body.inputDetails4}`
-  };
+    text: 'Hello World'
 
+
+  //      `${req.body.inputParentFirstName4} ${req.body.inputParentLastName4}
+  //   // ${req.body.inputStudentFirstName4} ${req.body.inputStudentLastName4}
+  //   // ${req.body.inputEmail4} ${req.body.phone4}
+  //   // ${req.body.grade4} ${req.body.inputMathSubject4}
+  //   // ${req.body.inputDetails4}`
+  //
+
+  }
   mailgun.messages().send(data, function (error, body) {
-    console.log(body);
-    if(!error){
-      res.send("Form Successfully Submitted. I will be in contact with you soon!")
-    } else {
-      res.send("Form submission unsuccessful. Please try again later.")
-    }
+      if(!error){
+        res.send(data)
+        console.log(data);
+
+      } else {
+        console.log(error);
+        res.send("Form submission unsuccessful. Please try again later.")
+      }
+    });
   });
-});
+
 
 // app.post('/form', urlencodedParser, function (req, res){
 //   console.log(req.body);
